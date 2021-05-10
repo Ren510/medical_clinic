b2827b6d1541ff1b66704c07e840364e4462bb22
Ren510 <yt.1141977@gmail.com>
2021-05-10 12:30:17 +0000

    [Google Map API]about_view

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,4 +1,6 @@
 class Patient < ApplicationRecord
+  
+  # ユーザの新規登録で、郵便番号入力した際に住所自動入力するための記述
   include JpPrefecture
   jp_prefecture :prefecture_code
   
@@ -9,6 +11,8 @@ class Patient < ApplicationRecord
   def prefecture_name=(prefecture_name)
     self.prefecture_code = JpPrefecture::Prefecture.find(name: prefecture_name).code
   end
+  
+  # ユーザーのデバイス
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-
+  
+  # ユーザー側のデバイス
   devise_for :patients
   root 'homes#top'
   get 'about' => "homes#about"
-  
+  resources :maps, only: [:a]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
